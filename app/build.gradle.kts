600ce123e39f1f0bafd74f3334806f7048bd4a1e
sumon6hj-ctrl <sumon6hj@gmail.com>
2026-09-08 17:45:18 +0600

    Upload app/build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,5 @@
+plugins { id("com.android.application"); id("org.jetbrains.kotlin.android") }
+android { namespace = "com.sulav.proxy"; compileSdk = 35
+    defaultConfig { applicationId = "com.sulav.proxy"; minSdk = 24; targetSdk = 35; versionCode = 1; versionName = "1.0" }
+}
+kotlin { jvmToolchain(17) }
